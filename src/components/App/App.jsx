778eb1104f27124675fb28e23646f4448d735188
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-07-12 09:35:48 -0700

    added react-router-dom and a Home component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import CardForm from '../CardForm/CardForm';
+import Home from '../Home/Home';
 
 export default function App() {
   return (
     <>
-    <CardForm />
+    <Router>
+      <Switch>
+        <Link exact path ='/' component={Home}/>
+        <Link exact path ='/createCard' component={CardForm} />
+      </Switch>
+    </Router>
     </>
   );
 }
